494fcae572eb17efa99186daac739b07b1887f80
Edgar Morales <edgar.morales@softtek.com>
2020-01-13 10:42:21 -0600

    Adding a Migration to update MembershipType Name Field

---
+++ Vidly/Migrations/202001131639059_UpdateMembershipTypeNames.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateMembershipTypeNames : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"UPDATE MembershipTypes SET Name = 'Pay as you go' WHERE SignUpFee = 0");
+            Sql(@"UPDATE MembershipTypes SET Name = 'Monthly' WHERE SignUpFee = 30");
+            Sql(@"UPDATE MembershipTypes SET Name = 'Quarterly' WHERE SignUpFee = 90");
+            Sql(@"UPDATE MembershipTypes SET Name = 'Annual go' WHERE SignUpFee = 300");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
